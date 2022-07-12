08f9ce52a85ac5db5f5325f29fc354df569f96dc
OliveiraRodneyLucas <rodney.lagoinha@gmail.com>
2022-07-12 14:01:50 -0300

    Create a new file
    Operadores Aritméticos - Python

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite um valor:'))
+n2 = int(input('Digite outro valores: '))
+s = n1 +n2
+m = n1*n2
+d = n1/n2
+e = n1**n2
+di = n1//n2
+r = n1%n2
+print ('A soma dos valores é {}, a multiplicação é {}, a divisão é {:.3f}!'.format(s, m, d))
+print ('A expo é {}, a divisão inteira é {}, o resto é {} '.format(e, di, r))
+
